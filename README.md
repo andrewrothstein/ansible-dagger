9b9dfbba5ecc02db04fed4f691dbdad8598f26ed
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-08-27 23:07:25 -0400

    dagger:0.2.31

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.dagger
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-dagger/actions/workflows/build.yml/badge.svg)
+
+Installs [dagger](https://dagger.io/).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.dagger
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://dagger-io.s3.amazonaws.com/dagger/releases
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="dagger_v${ver}_${platform}.${archive_type}"
+    local url=$MIRROR/$ver/$file
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep -e "$file\$" $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    local url=$MIRROR/$ver/checksums.txt
+    local lchecksums=$DIR/dagger-${ver}-checksums.txt
+    if [ ! -e $lchecksums ];
+    then
+        curl -sSLf -o $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums windows arm64 zip
+    dl $ver $lchecksums windows armv7 zip
+    dl $ver $lchecksums windows amd64 zip
+    dl $ver $lchecksums linux armv7
+    dl $ver $lchecksums linux arm64
+}
+
+dl_ver ${1:-0.2.31}
