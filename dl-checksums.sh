702bce78adff24e813d138101dace9d520f5e0fa
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-11-02 01:26:44 +0000

    dagger:0.9.2, drop pre 0.9, reseat mirror to github packages

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 DIR=~/Downloads
-MIRROR=https://dagger-io.s3.amazonaws.com/dagger/releases
+MIRROR=https://github.com/dagger/dagger/releases/download
 
 dl() {
     local ver=$1
@@ -11,14 +11,15 @@ dl() {
     local archive_type=${5:-tar.gz}
     local platform="${os}_${arch}"
     local file="dagger_v${ver}_${platform}.${archive_type}"
-    local url=$MIRROR/$ver/$file
+    local url=$MIRROR/v$ver/$file
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(grep -e "$file\$" $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
-    local url=$MIRROR/$ver/checksums.txt
+    # https://github.com/dagger/dagger/releases/download/v0.9.2/checksums.txt
+    local url=$MIRROR/v$ver/checksums.txt
     local lchecksums=$DIR/dagger-${ver}-checksums.txt
     if [ ! -e $lchecksums ];
     then
@@ -38,4 +39,4 @@ dl_ver() {
     dl $ver $lchecksums windows amd64 zip
 }
 
-dl_ver ${1:-0.9.1}
+dl_ver ${1:-0.9.2}
